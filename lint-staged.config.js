68b63e0f837acaef947b19127961bb3a123e6150
wangbo <99548050@qq.com>
2026-03-03 16:51:12 +0800

    feat: :sparkles: lint相关配置优化

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@ export default {
   '*.{js,jsx,ts,tsx,vue}': ['prettier --write', 'oxlint --fix', 'eslint --fix'],
   '!(package)*.json': ['prettier --write --parser json'],
   'package.json': ['prettier --write'],
-  '*.{css,scss,less,styl,html}': ['stylelint --fix --allow-empty-input', 'prettier --write'],
+  '*.{css,scss,less,styl,html}': ['prettier --write', 'stylelint --fix --allow-empty-input'],
   '*.vue': ['stylelint --fix --allow-empty-input'],
   '*.md': ['prettier --write'],
 }
